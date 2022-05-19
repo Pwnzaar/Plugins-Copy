6a7a4191724b72e9bc71d02eadfc6ac2f4b270ef
Pwnzaar <pwnzaar@gmail.com>
2022-05-20 00:45:55 +0100

    Update1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-rootProject.name = "Kitsch Plugins"
+rootProject.name = "Pwn Plugins"
 
 include(":birdhouseinfobox")
 include(":coxraidscouter")
